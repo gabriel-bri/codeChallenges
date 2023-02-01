066dff93dc4a570ac465063545500a66005751e2
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2023-02-01 13:49:55 -0300

    Delete representantesExecutivos.sql

--- Beecrowd/representantesExecutivos.sql
+++
@@ -1,7 +0,0 @@
-/**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
- */
-
-SELECT products.name, providers.name FROM products INNER JOIN providers ON products.id_providers = providers.id INNER JOIN categories ON products.id_categories = categories.id WHERE categories.id = 6;
