f9ea770d2fe0166bc59204a82caa1658030660a7
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2023-02-22 15:32:15 -0300

    Solução do desafio usando Java

---
+++ Testes técnicos/Lumni Educação/2 - Parênteses balanceados/parenteses_balanceados.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        String s =  new String("()()");;
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char letra = s.charAt(i);
+            
+            if(letra == '(') {
+                stack.push(letra);
+            }
+            
+            else {
+                if(stack.size() > 0 && stack.peek() == '(') {
+                    stack.pop();
+                }
+                
+                else {
+                    stack.push(letra);
+                }
+            }
+        }
+        
+        System.out.println(stack.size());
+        // System.out.println("faz o l");
+    }
+}
